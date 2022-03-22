39c9ef3efce55af4cc3fec63c3c7c6728b11f54e
vudat2406 <vudat2462000@gmail.com>
2022-03-22 17:40:31 +0700

    sua

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -8,25 +8,69 @@ class DrawerItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(15, 7.5, 15, 7.5),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            image,
-            height: 20,
-            width: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 7.5),
-            child: Text(
-              name,
-              style: Theme.of(context).textTheme.overline,
+    var annouceCount = 15;
+    return name == "Projects"
+        ? Container(
+            padding: const EdgeInsets.fromLTRB(15, 7.5, 15, 7.5),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Image.asset(
+                      image,
+                      height: 20,
+                      width: 20,
+                    ),
+                    Positioned(
+                        top: -5,
+                        right: -7,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromRGBO(133, 37, 43, 1),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(4, 0, 4, 4),
+                            child: Text(
+                              '$annouceCount',
+                              style: const TextStyle(
+                                  fontSize: 10, color: Colors.white),
+                            ),
+                          ),
+                        ))
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 7.5),
+                  child: Text(
+                    name,
+                    style: Theme.of(context).textTheme.overline,
+                  ),
+                )
+              ],
             ),
           )
-        ],
-      ),
-    );
+        : Container(
+            padding: const EdgeInsets.fromLTRB(15, 7.5, 15, 7.5),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  image,
+                  height: 20,
+                  width: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 7.5),
+                  child: Text(
+                    name,
+                    style: Theme.of(context).textTheme.overline,
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
