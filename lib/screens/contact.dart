b3b9cd62cb19e3f31bc544eaa700d4f5bb575a9e
vudat2406 <vudat2462000@gmail.com>
2022-03-29 13:52:58 +0700

    paging

--- lib/screens/contact.dart
+++ lib/screens/contact.dart
@@ -1,4 +1,5 @@
 export 'contact.dart';
+import 'dart:async';
 import 'dart:js';
 
 import 'package:flutter/material.dart';
@@ -11,11 +12,22 @@ import 'package:test/widgets/allwidgets.dart';
 import '../data/models/contact.dart';
 
 class ContactsScreen extends StatelessWidget {
-  const ContactsScreen({Key key}) : super(key: key);
+  // const ContactsScreen({Key key}) : super(key: key);
+  final scrollController = ScrollController();
+  void setupScrollController(context) {
+    scrollController.addListener(() {
+      if (scrollController.position.atEdge) {
+        if (scrollController.position.pixels != 0) {
+          BlocProvider.of<ContactCubit>(context).LoadContact();
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    BlocProvider.of<ContactCubit>(context).fetchContact();
+    setupScrollController(context);
+    BlocProvider.of<ContactCubit>(context).LoadContact();
     return Scaffold(
       drawer: SizedBox(
           width: 164,
@@ -90,9 +102,16 @@ class ContactsScreen extends StatelessWidget {
       appBar: const Apptopbar(),
       body: BlocBuilder<ContactCubit, ContactState>(
         builder: (context, state) {
-          if (!(state is ContactLoaded))
-            return Center(child: CircularProgressIndicator());
-          final contacts = (state as ContactLoaded).contacts;
+          if (state is ContactLoading && state.isFirstFetch)
+            return _loadingIndicator();
+          List<Contact> contacts = [];
+          bool isLoading = false;
+          if (state is ContactLoading) {
+            contacts = state.oldContacts;
+            isLoading = true;
+          } else if (state is ContactLoaded) {
+            contacts = state.contacts;
+          }
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Column(
@@ -101,19 +120,31 @@ class ContactsScreen extends StatelessWidget {
                   nameScreen: 'Contacts',
                   isback: false,
                 ),
+                // Expanded(
+                //   child: SingleChildScrollView(
+                //     child: Column(
+                //       children: contacts.map((e) => CardContact(e)).toList(),
+                //     ),
+                //   ),
+                // ),
                 Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: contacts.map((e) => CardContact(e)).toList(),
-                    ),
+                  child: ListView.builder(
+                    controller: scrollController,
+                    shrinkWrap: true,
+                    itemCount: contacts.length + (isLoading ? 1 : 0),
+                    itemBuilder: (context, index) {
+                      if (index < contacts.length)
+                        return CardContact(contacts[index]);
+                      else {
+                        Timer(Duration(microseconds: 300), () {
+                          scrollController.jumpTo(
+                              scrollController.position.maxScrollExtent);
+                        });
+
+                        return _loadingIndicator();
+                      }
+                    },
                   ),
-                  // child: ListView.builder(
-                  //   shrinkWrap: true,
-                  //   itemCount: DataContact.datacontacts.length,
-                  //   itemBuilder: (context, index) {
-                  //     return CardContact(data: DataContact.datacontacts[index]);
-                  //   },
-                  // ),
                 )
               ],
             ),
@@ -133,96 +164,103 @@ class ContactsScreen extends StatelessWidget {
     );
   }
 
-  Widget _Contact(Contact contact, context) {
-    return InkWell(
-      onTap: () =>
-          Navigator.pushNamed(context, CONTACT_DETAIL, arguments: contact),
-      child: Container(
-        key: Key("${contact.id}"),
-        child: Container(
-          // key: Key("${widget.contact.id}"),
-          height: 65,
-          decoration: BoxDecoration(
-              color:
-                  // isShowOption
-                  //     ? const Color.fromRGBO(242, 249, 205, 1)
-                  Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.08),
-                  offset: const Offset(0.5, 0.5),
-                  blurRadius: 1.5,
-                  spreadRadius: 0.5,
-                )
-              ],
-              borderRadius: BorderRadius.circular(3)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Expanded(
-                  flex: 7,
-                  child: Row(children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 7.5),
-                      child: CircleAvatar(
-                          // backgroundImage:
-                          //     AssetImage(widget.data.avatarImage)
-                          ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            contact.contactName,
-                            style: Theme.of(context).textTheme.headline5,
-                          ),
-                          Row(
-                            children: [
-                              contact.contactPhoneNum1 != null
-                                  ? Text(
-                                      contact.contactPhoneNum1,
-                                      // "widget.data.phoneNumber",
-                                      style:
-                                          Theme.of(context).textTheme.bodyText2,
-                                    )
-                                  : Text("no phone"),
-                              const Text('  |  '),
-                              contact.contactAdr1 != null
-                                  ? Text(
-                                      contact.contactAdr1,
-                                      style:
-                                          Theme.of(context).textTheme.bodyText2,
-                                    )
-                                  : Text("none")
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ])),
-              // Expanded(
-              //   flex: 1,
-              //   child: IconButton(
-              //     onPressed: () {
-              //       setState(() {
-              //         isShowOption = !isShowOption;
-              //       });
-              //     },
-              //     icon: Image.asset(
-              //       'assets/images/ad.png',
-              //       color: isShowOption
-              //           ? const Color.fromRGBO(184, 161, 91, 1)
-              //           : const Color.fromRGBO(202, 202, 202, 1),
-              //     ),
-              //   ),
-              // )
-            ],
-          ),
-        ),
-      ),
+  // Widget _Contact(Contact contact, context) {
+  //   return InkWell(
+  //     onTap: () =>
+  //         Navigator.pushNamed(context, CONTACT_DETAIL, arguments: contact),
+  //     child: Container(
+  //       key: Key("${contact.id}"),
+  //       child: Container(
+  //         // key: Key("${widget.contact.id}"),
+  //         height: 65,
+  //         decoration: BoxDecoration(
+  //             color:
+  //                 // isShowOption
+  //                 //     ? const Color.fromRGBO(242, 249, 205, 1)
+  //                 Colors.white,
+  //             boxShadow: [
+  //               BoxShadow(
+  //                 color: Colors.black.withOpacity(0.08),
+  //                 offset: const Offset(0.5, 0.5),
+  //                 blurRadius: 1.5,
+  //                 spreadRadius: 0.5,
+  //               )
+  //             ],
+  //             borderRadius: BorderRadius.circular(3)),
+  //         child: Row(
+  //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //           children: [
+  //             Expanded(
+  //                 flex: 7,
+  //                 child: Row(children: [
+  //                   Padding(
+  //                     padding: const EdgeInsets.only(left: 7.5),
+  //                     child: CircleAvatar(
+  //                         // backgroundImage:
+  //                         //     AssetImage(widget.data.avatarImage)
+  //                         ),
+  //                   ),
+  //                   Padding(
+  //                     padding: const EdgeInsets.only(left: 5),
+  //                     child: Column(
+  //                       crossAxisAlignment: CrossAxisAlignment.start,
+  //                       mainAxisAlignment: MainAxisAlignment.center,
+  //                       children: [
+  //                         Text(
+  //                           contact.contactName,
+  //                           style: Theme.of(context).textTheme.headline5,
+  //                         ),
+  //                         Row(
+  //                           children: [
+  //                             contact.contactPhoneNum1 != null
+  //                                 ? Text(
+  //                                     contact.contactPhoneNum1,
+  //                                     // "widget.data.phoneNumber",
+  //                                     style:
+  //                                         Theme.of(context).textTheme.bodyText2,
+  //                                   )
+  //                                 : Text("no phone"),
+  //                             const Text('  |  '),
+  //                             contact.contactAdr1 != null
+  //                                 ? Text(
+  //                                     contact.contactAdr1,
+  //                                     style:
+  //                                         Theme.of(context).textTheme.bodyText2,
+  //                                   )
+  //                                 : Text("none")
+  //                           ],
+  //                         )
+  //                       ],
+  //                     ),
+  //                   ),
+  //                 ])),
+  //             // Expanded(
+  //             //   flex: 1,
+  //             //   child: IconButton(
+  //             //     onPressed: () {
+  //             //       setState(() {
+  //             //         isShowOption = !isShowOption;
+  //             //       });
+  //             //     },
+  //             //     icon: Image.asset(
+  //             //       'assets/images/ad.png',
+  //             //       color: isShowOption
+  //             //           ? const Color.fromRGBO(184, 161, 91, 1)
+  //             //           : const Color.fromRGBO(202, 202, 202, 1),
+  //             //     ),
+  //             //   ),
+  //             // )
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  Widget _loadingIndicator() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(child: CircularProgressIndicator()),
     );
   }
 }
